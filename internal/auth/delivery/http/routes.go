9411aff6b53e3290bffa17b54e242ed4d9652074
AnhDung <dungnaph52171@gmail.com>
2025-02-28 15:09:34 +0700

    init feature authentication

--- internal/auth/delivery/http/routes.go
+++ internal/auth/delivery/http/routes.go
@@ -8,10 +8,10 @@ import (
 
 func MapAuthRoutes(authGroup *gin.RouterGroup, h auth.Handler) {
 	authGroup.POST("/register", h.Register())
-	// authGroup.POST("/login", h.Login())
-	// authGroup.POST("/logout", h.Logout())
-	// authGroup.POST("/register", h.Register())
-	// authGroup.GET("/find", h.FindByName())
-	// authGroup.GET("/all", h.GetUsers())
-	// authGroup.GET("/:user_id", h.GetUserByID())
+	authGroup.POST("/login", h.Login())
+	authGroup.POST("/logout", h.Logout())
+	authGroup.POST("/register", h.Register())
+	authGroup.GET("/find", h.FindByName())
+	authGroup.GET("/all", h.GetUsers())
+	authGroup.GET("/:user_id", h.GetUserByID())
 }
--- internal/auth/repository/pg_repository.go
+++ internal/auth/repository/pg_repository.go
@@ -2,17 +2,16 @@ package repository
 
 import (
 	"context"
-	// "database/sql"
+	"database/sql"
 	auth "health_backend/internal/auth/interfaces"
 	"health_backend/internal/models"
 
-	// "health_backend/pkg/utils"
-	// "time"
+	"health_backend/pkg/utils"
 
 	"github.com/pkg/errors"
 	"gorm.io/gorm"
 
-	// uuid "github.com/jackc/pgx/pgtype/ext/satori-uuid"
+	uuid "github.com/jackc/pgx/pgtype/ext/satori-uuid"
 	opentracing "github.com/opentracing/opentracing-go"
 )
 
@@ -28,52 +27,52 @@ func NewAuthRepository(db *gorm.DB) auth.Repository {
 	}
 }
 
-// // Delete implements auth.Repository.
-// func (r *authRepo) Delete(ctx context.Context, userID uuid.UUID) error {
-// 	span, ctx := opentracing.StartSpanFromContext(ctx, "authRepo.DeleteUser")
-// 	defer span.Finish()
+// Delete implements auth.Repository.
+func (r *authRepo) Delete(ctx context.Context, userID uuid.UUID) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "authRepo.DeleteUser")
+	defer span.Finish()
 
-// 	result := r.db.WithContext(ctx).Where("id = ? ", userID).Delete(&models.User{})
-// 	if result.Error != nil {
-// 		return errors.Wrap(result.Error, "authRepo.DeleteUser")
-// 	}
-// 	if result.RowsAffected == 0 {
-// 		return errors.Wrap(sql.ErrNoRows, "authRepo.DeleteUser")
-// 	}
-// 	return nil
-// }
+	result := r.db.WithContext(ctx).Where("id = ? ", userID).Delete(&models.User{})
+	if result.Error != nil {
+		return errors.Wrap(result.Error, "authRepo.DeleteUser")
+	}
+	if result.RowsAffected == 0 {
+		return errors.Wrap(sql.ErrNoRows, "authRepo.DeleteUser")
+	}
+	return nil
+}
 
-// // TODO :FinadByEmail implementaion
-// func (r *authRepo) FindByEmail(ctx context.Context, user *models.User) (*models.User, error) {
-// 	span, ctx := opentracing.StartSpanFromContext(ctx, "authRepo.FinByEmail")
-// 	defer span.Finish()
+// TODO :FinadByEmail implementaion
+func (r *authRepo) FindByEmail(ctx context.Context, user *models.User) (*models.User, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "authRepo.FinByEmail")
+	defer span.Finish()
 
-// 	result := r.db.WithContext(ctx).Where("email= ?", user.Email).First(&user)
-// 	if result.Error != nil {
-// 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-// 			return nil, errors.Wrap(gorm.ErrRecordNotFound, "user not found")
-// 		}
-// 		return nil, errors.Wrap(result.Error, "authRepo.FindByEmail")
-// 	}
+	result := r.db.WithContext(ctx).Where("email= ?", user.Email).First(&user)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, errors.Wrap(gorm.ErrRecordNotFound, "user not found")
+		}
+		return nil, errors.Wrap(result.Error, "authRepo.FindByEmail")
+	}
 
-// 	return user, nil
+	return user, nil
 
-// }
+}
 
-// // TODO: FindByName implements auth.Repository.
-// func (r *authRepo) FindByName(ctx context.Context, name string, query *utils.PaginationQuery) (*models.UsersList, error) {
-// 	panic("unimplemented")
-// }
+// TODO: FindByName implements auth.Repository.
+func (r *authRepo) FindByName(ctx context.Context, name string, query *utils.PaginationQuery) (*models.UsersList, error) {
+	panic("unimplemented")
+}
 
-// // TODO : GetByID implements auth.Repository.
-// func (r *authRepo) GetByID(ctx context.Context, userID uuid.UUID) (*models.User, error) {
-// 	panic("unimplemented")
-// }
+// TODO : GetByID implements auth.Repository.
+func (r *authRepo) GetByID(ctx context.Context, userID uuid.UUID) (*models.User, error) {
+	panic("unimplemented")
+}
 
-// // TODO: GetUsers implements auth.Repository.
-// func (r *authRepo) GetUsers(ctx context.Context, pq *utils.PaginationQuery) (*models.UsersList, error) {
-// 	panic("unimplemented")
-// }
+// TODO: GetUsers implements auth.Repository.
+func (r *authRepo) GetUsers(ctx context.Context, pq *utils.PaginationQuery) (*models.UsersList, error) {
+	panic("unimplemented")
+}
 
 // Register implements auth.Repository.
 func (r *authRepo) Register(ctx context.Context, user *models.User) (*models.User, error) {
@@ -87,39 +86,9 @@ func (r *authRepo) Register(ctx context.Context, user *models.User) (*models.Use
 	return user, nil
 }
 
-// // Update implements auth.Repository.
-// func (r *authRepo) Update(ctx context.Context, user *models.User) (*models.User, error) {
-// 	span, ctx := opentracing.StartSpanFromContext(ctx, "authRepo.Update")
-// 	defer span.Finish()
+// Update implements auth.Repository.
+func (r *authRepo) Update(ctx context.Context, user *models.User) (*models.User, error) {
 
-// 	tx := r.db.WithContext(ctx).Begin()
+	panic("unimplemented")
 
-// 	var existingUser *models.User
-// 	if err := tx.First(&existingUser, "user_id = ?", user.UserID).Error; err != nil {
-// 		tx.Rollback()
-// 		return nil, errors.Wrap(err, "authRepo.Update.First")
-// 	}
-// 	if err := tx.Model(&existingUser).Updates(models.User{
-// 		FirstName:   user.FirstName,
-// 		LastName:    user.LastName,
-// 		Email:       user.Email,
-// 		Role:        user.Role,
-// 		About:       user.About,
-// 		Avatar:      user.Avatar,
-// 		PhoneNumber: user.PhoneNumber,
-// 		Address:     user.Address,
-// 		City:        user.City,
-// 		Gender:      user.Gender,
-// 		Postcode:    user.Postcode,
-// 		Birthday:    user.Birthday,
-// 		UpdatedAt:   time.Now(),
-// 	}).Error; err != nil {
-// 		tx.Rollback()
-// 		return nil, errors.Wrap(err, "authRepo.Update.Updates")
-// 	}
-
-// 	if err := tx.Commit().Error; err != nil {
-// 		return nil, errors.Wrap(err, "authRepo.Update.Commit")
-// 	}
-// 	return existingUser, nil
-// }
+}
